bc44d606cdbd42d1672876de1554bb7f1ed695b7
Andres Parra <ca.parraa@javeriana.edu.co>
2018-02-16 00:07:53 -0500

    Initial Commit

---
+++ GitCalculator/src/co/edu/javeriana/ingsw/ICalculator.java
@@ -0,0 +1,10 @@
+package co.edu.javeriana.ingsw;
+
+public interface ICalculator {
+	
+	public int sumar(int a, int b);
+	public int restar (int a, int b);
+	public int multiplicar(int a, int b);
+	public double dividir(int a, int b);
+
+}
---
+++ GitCalculator/src/co/edu/javeriana/ingsw/MainClass.java
@@ -0,0 +1,12 @@
+package co.edu.javeriana.ingsw;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		ICalculator calculator;
+		//calculator=new ...
+		//System.out.println("5 + 6="+calculator.sumar(5, 6));
+
+	}
+
+}
