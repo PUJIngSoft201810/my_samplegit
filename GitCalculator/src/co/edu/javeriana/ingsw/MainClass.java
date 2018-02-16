7a94d98b1a5b92b785d05b3ae565a37176c5882d
Andres Parra <ca.parraa@javeriana.edu.co>
2018-02-16 01:14:15 -0500

    Add comments for all operations

--- GitCalculator/src/co/edu/javeriana/ingsw/MainClass.java
+++ GitCalculator/src/co/edu/javeriana/ingsw/MainClass.java
@@ -4,9 +4,18 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		ICalculator calculator;
-		//calculator=new ...
+		//suma
+		//calculator=new Suma...
 		//System.out.println("5 + 6="+calculator.sumar(5, 6));
-
+		
+		//resta
+		//calculator = new Resta...
+		//System.out.println("10 - 4="+calculator.restar(10, 4));
+		
+		//multiplicación
+		
+		//división
+		
 	}
 
 }
